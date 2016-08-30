2fd8c9d890845c8e9ab67595b3cff05bdcf45947
Francesco Borg Bonaci <frellibb@gmail.com>
2016-08-30 20:42:54 +0200

    Added repository test

--- cookie-solutions/src/main/test/neurobics/gym/cookie/solutions/CookieSolutionsAPITest.java
+++ cookie-solutions/src/main/test/neurobics/gym/cookie/solutions/CookieSolutionsAPITest.java
@@ -1,5 +1,19 @@
 package neurobics.gym.cookie.solutions;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class CookieSolutionsAPITest {
+
+    private CookieRepository cookieRepository;
+
+    @Test
+    public void cookieRepository_getCookies_returnsAllCookies() {
+        List<Cookie> cookies = cookieRepository.findAll();
+
+        
+    }
 
 }
