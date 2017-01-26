67599fdad236321af60598e8e36491a34f04112d
Francesco Borg Bonaci <frellibb@gmail.com>
2017-01-26 21:49:26 +0100

    Infixgit pull

--- kotlin-funtime/src/main/kotlin/neurobics.gym.kotlin/HelloWorld.kt
+++ kotlin-funtime/src/main/kotlin/neurobics.gym.kotlin/HelloWorld.kt
@@ -1,18 +1,18 @@
 package neurobics.gym.kotlin
 
 fun tisTheBuzz(n: Int): String {
-    return if (n% 15 == 0) "fizz buzz" else if (n%5 == 0) "buzz" else if (n%3 == 0) "fizz" else "$n"
+
+    return when {
+        divisibleBy(n, 15) -> "fizzbuzz"
+        divisibleBy(n, 3) -> "fizz"
+        divisibleBy(n, 5) -> "buzz"
+        else -> "$n"
+    }
 }
 
+infix fun Int.divisibleBy(m: Int) = this % m == 0
 
 fun main(args: Array<String>) {
-    // n
-    // n div by 3 fizz
-    // n div by 5 buzz
-    // n div by 3 and 5 fizz buzz
-    // ...
-    // profit
-
     for (i in 1..16)
         println("$i = ${tisTheBuzz(i)}")
 }
