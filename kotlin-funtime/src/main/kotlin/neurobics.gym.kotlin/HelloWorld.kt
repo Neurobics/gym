f631c66d8908923f8dc74c69039f3e3a52aee767
Luke Camilleri <mail@camilleriluke.com>
2017-01-26 21:35:10 +0100

    feat(🐦): KOTLIN!!!!!! 🎉🌽

--- kotlin-funtime/src/main/kotlin/neurobics.gym.kotlin/HelloWorld.kt
+++ kotlin-funtime/src/main/kotlin/neurobics.gym.kotlin/HelloWorld.kt
@@ -1,5 +1,18 @@
 package neurobics.gym.kotlin
 
+fun tisTheBuzz(n: Int): String {
+    return if (n% 15 == 0) "fizz buzz" else if (n%5 == 0) "buzz" else if (n%3 == 0) "fizz" else "$n"
+}
+
+
 fun main(args: Array<String>) {
-    println("Start the warmup!")
+    // n
+    // n div by 3 fizz
+    // n div by 5 buzz
+    // n div by 3 and 5 fizz buzz
+    // ...
+    // profit
+
+    for (i in 1..16)
+        println("$i = ${tisTheBuzz(i)}")
 }
